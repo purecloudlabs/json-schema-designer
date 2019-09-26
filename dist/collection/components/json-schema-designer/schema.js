875c14f5d30c04a191114eab472b8411198aa1b0
Jordan Stith <jordan.stith@genesys.com>
2019-09-26 14:01:48 -0500

    Revert "version 0.0.15"
    This reverts commit 5ada7ef3

--- dist/collection/components/json-schema-designer/schema.js
+++ dist/collection/components/json-schema-designer/schema.js
@@ -20,7 +20,6 @@ export function createAppropriateSchemaItem(json, parent) {
             case 'string':
                 return new SchemaString(json, parent);
             case 'integer':
-                return new SchemaInteger(json, parent);
             case 'number':
                 return new SchemaNumeric(json, parent);
             default:
@@ -238,28 +237,6 @@ export class SchemaNumeric extends SchemaBasic {
     constructor(json, parent) {
         super(json, parent);
         this._appropriateTypes = ['number', 'integer'];
-        this.multipleOf = json.multipleOf;
-        this.minimum = json.minimum;
-        this.exclusiveMinimum = json.exclusiveMinimum;
-        this.maximum = json.maximum;
-        this.exclusiveMaximum = json.exclusiveMaximum;
-        this.setAppropriateType();
-        this.checkAppropriateType();
-    }
-    jsonSchema() {
-        let output = super.jsonSchema();
-        output.multipleOf = this.multipleOf ? this.multipleOf : undefined;
-        output.minimum = this.minimum ? this.minimum : undefined;
-        output.exclusiveMinimum = this.exclusiveMinimum ? this.exclusiveMinimum : undefined;
-        output.maximum = this.maximum ? this.maximum : undefined;
-        output.exclusiveMaximum = this.exclusiveMaximum ? this.exclusiveMaximum : undefined;
-        return output;
-    }
-}
-export class SchemaInteger extends SchemaNumeric {
-    constructor(json, parent) {
-        super(json, parent);
-        this._appropriateTypes = ['integer'];
         this.multipleOf = json.multipleOf;
         this.minimum = json.minimum;
         this.exclusiveMinimum = json.exclusiveMinimum;
